632aa178044a928643ed79af53d9c985b7336f5b
Gord Tanner <gtanner@gmail.com>
2012-04-23 10:56:55 -0400

    updated license information

--- server/app.js
+++ server/app.js
@@ -1,3 +1,18 @@
+/*
+ *  Copyright 2011 Research In Motion Limited.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 var express = require('express'),
     app = express.createServer(), 
     io = require('socket.io').listen(app),
@@ -9,16 +24,6 @@ app.configure(function () {
 
 app.get('/', function (req, res) {
     res.sendfile(__dirname + '/public/index.html');
-});
-
-
-app.get('/images', function (req, res) {
-    var images = require("./images"),
-        tiles = images.all("tile");
-
-    res.send(images.all("tile").concat(images.all("full")).map(function (tile) {
-        return "<img src='" + tile + "'/>";
-    }).join(''));
 });
 
 io.sockets.on('connection', function (socket) {
